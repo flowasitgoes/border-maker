06f220dc305b91e713e867e96e61c05e2ca16fe0
Flow <guanjun211@gmail.com>
2025-11-17 18:30:26 +0800

    重新上傳按鈕改好重新上傳按鈕改好

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { BorderService } from '../services/border.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss']
 })
-export class HomePage {
-  constructor() {}
+export class HomePage implements OnInit, OnDestroy {
+  uploadedImage: string | null = null;
+  private subscriptions: Subscription[] = [];
+
+  constructor(private borderService: BorderService) {}
+
+  ngOnInit(): void {
+    const imageSub = this.borderService.uploadedImage$.subscribe(image => {
+      this.uploadedImage = image;
+    });
+    this.subscriptions.push(imageSub);
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(sub => sub.unsubscribe());
+  }
+
+  resetUpload(): void {
+    this.borderService.setUploadedImage(null);
+  }
 }
 
